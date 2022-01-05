ed15622d401926beb2fc871a9170f3b1450ba2b2
Vaibhav Ankolekar <76490924+RagnosVA@users.noreply.github.com>
2022-01-05 14:38:48 +0530

    socket hostname

--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -1,5 +1,9 @@
 let connected = false;
 
+console.log("protocol : " + window.location.protocol);
+console.log("host : " + window.location.host);
+console.log("hostname : " + window.location.hostname);
+
 let socket = io("http://localhost:5000");
 
 socket.emit("setup", userLoggedIn);
